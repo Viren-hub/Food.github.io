7a0e2c0038adb3a79c5356bc1f0e2f1eb7050bd5
virendra <virendragayakwad11@gmail.com>
2021-04-20 10:43:02 +0530

    add comments :by Virendra

--- FoodOrder/order.js
+++ FoodOrder/order.js
@@ -1,5 +1,7 @@
 let allVeg = localStorage.getItem("veg") ? JSON.parse(localStorage.getItem("veg")) : []
-
+let arr = [];
+let product = [];
+//=========================== Inserting new Stock ==================================//
 function addStock() {
     let data = {
         "id": document.getElementById("id").value,
@@ -13,15 +15,15 @@ function addStock() {
     localStorage.setItem("veg", JSON.stringify(allVeg));
     ShowVeg();
 }
-
+//========================= Show all vagitables data ================================//
 ShowVeg = () => {
-    let allVeg = localStorage.getItem ? JSON.parse(localStorage.getItem("veg")) : []
-    let showAll = ""
-    let obj = allVeg.find(e => e.name == "test2")
+        let allVeg = localStorage.getItem ? JSON.parse(localStorage.getItem("veg")) : []
+        let showAll = ""
+        let obj = allVeg.find(e => e.name == "test2")
 
-    allVeg.forEach(element => {
-        if ((+element.quantity) > 0) {
-            showAll += `<div class="images" id="${element.id}">
+        allVeg.forEach(element => {
+            if ((+element.quantity) > 0) {
+                showAll += `<div class="images" id="${element.id}">
             <img src="${element.Image}" alt="Image">
     
             <div class="sub-img">
@@ -32,12 +34,12 @@ ShowVeg = () => {
                 <input class="btn" type="button" id="buy" value="Buy Now" onclick="reduceQty(${element.id})" />
             </div>
         </div>`
-        }
-    });
-    document.getElementById("buyer").innerHTML = showAll;
-    UpdateItemList();
-}
-
+            }
+        });
+        document.getElementById("buyer").innerHTML = showAll;
+        UpdateItemList();
+    }
+    //============================ Update stock HTML Templete ================================//
 UpdateItemList = () => {
     let allVeg = localStorage.getItem ? JSON.parse(localStorage.getItem("veg")) : []
     let showAll = ""
@@ -55,42 +57,30 @@ UpdateItemList = () => {
                 <input class="btn" type="button" id="buy" value="Update Stock" onclick="addQty(${element.id})" /></br>
             </div>
         </div>`
-        console.log(element.id)
+
     });
 
-    //showAll+=``
+
     document.getElementById("seller").innerHTML = showAll;
 }
 
-// function cart(id) {
-//     let showAll = ""
-//     let cartItem = document.getElementById(`cart${id}`).value;
-//     allVeg.forEach(element => {
-//             showAll += `<div class="images" id="${element.id}">
-//         <img src="${element.Image}" alt="Image">
-//             <div class="sub-img">
-//                 <h3>Name: ${element.name}</h3>
-//                 <h3>Price: RS ${element.price}per Kg </h3>
-//                 <input type="number" value="${element.quantity}" id=addQuantity${element.id}>
-//                 <input class="btn" type="button" id="buy" value="Update Stock" onclick="addQty(${element.id})" /></br>
-//             </div>
-//         </div>`
-//             document.getElementById("cart-item").innerHTML = showAll;
-//         }
-//     }
+function cart(id) {
+    alert(arr[0].id)
+}
 
-
+//============================ Buy product and reduct quantity of data===================//
 function reduceQty(id) {
-    var arr = [];
+
     let buyQty = document.getElementById(`buyQuantity${id}`).value;
     let allVeg = localStorage.getItem ? JSON.parse(localStorage.getItem("veg")) : []
-    let product = allVeg.filter(ele => ele.id == id)
-    console.log("reduceQty=====", typeof(product[0].quantity), "buyqty==", buyQty, "id===", id)
+    product = allVeg.filter(ele => ele.id == id)
+    arr.push(product)
     product[0].quantity = product[0].quantity >= buyQty ? parseInt(product[0].quantity) - buyQty : alert(`please reduce the quatity to ${product[0].quantity}`)
     localStorage.setItem("veg", JSON.stringify(allVeg));
     ShowVeg();
+    cart()
 }
-
+//============================ Add stock in existing stock ================================//
 function addQty(id) {
     let buyQty = document.getElementById(`addQuantity${id}`).value
     let allVeg = localStorage.getItem ? JSON.parse(localStorage.getItem("veg")) : []
